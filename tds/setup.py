13eeec565c0a490930b9c2632d502e1c2078ec0d
Fumihiro Bessho (Ben) <fumihiro.bessho@gmail.com>
2016-03-10 03:42:15 +0900

    Add tds/setup.py

---
+++ tds/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tds',
+    version='0.1.0',
+    description='TDS',
+    long_description='TDS',
+    url='https://github.com/fbessho/empty-packages/tds',
+    author='Fumihiro Bessho',
+    author_email='iwabako@gmail.com',
+    classifiers=[],
+)
